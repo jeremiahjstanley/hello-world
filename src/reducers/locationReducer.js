47d4b3c410b731e4abfe14537c7c52d886f6cbbb
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-21 12:03:03 -0600

    Create locationReducer

---
+++ src/reducers/locationReducer.js
@@ -0,0 +1,18 @@
+
+export const hasErrored = (state = false, action) => {
+  switch(action.type) {
+    case 'HAS_ERRORED':
+      return action.hasErrored
+    default:
+      return state;
+  }
+}
+
+export const initialLocation = (state = {}, action) => {
+  switch(action.type) {
+    case 'FETCH_INITIAL_LOCATION_SUCCESS':
+      return action.initialLocation;
+    default: 
+      return state;
+  }
+}
