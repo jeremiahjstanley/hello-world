21a84e7a21be54198cbdc47858bf80fde820324f
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-22 13:02:34 -0600

    Add filter prototype to convert country name to isoAlpha3 code

--- src/thunks/setInitialLocation.js
+++ src/thunks/setInitialLocation.js
@@ -1,18 +1,19 @@
 import { fetchInitialLocationSuccess} from '../actions';
+import { goveranceIndicatorCleaner } from '../helper/dataCleaner'
 import { fetchEstimates } from './fetchEstimates';
 import { fetchNumberOfSources } from './fetchNumberOfSources'; 
 import { fetchPercentileRank } from './fetchPercentileRank';
 import { fetchStandardError } from './fetchStandardError';
+import { countries } from '../helper/countryMetrics';
 
 export const setInitialLocation = (location, dataSet, dataBase) => {
-  // convert full location into ISO ALPHA-3 // 
-  const isoAlpha3 = location;
+  const isoAlpha3 = countries.find(country => location === country.name).alpha_3;
   return async (dispatch) => {
     const estimates = await dispatch(fetchEstimates(isoAlpha3, dataSet, dataBase));
     const numberOfSources = await dispatch(fetchNumberOfSources(isoAlpha3, dataSet, dataBase));
     const percentileRank = await dispatch(fetchPercentileRank(isoAlpha3, dataSet, dataBase));
     const standardError = await dispatch(fetchStandardError(isoAlpha3, dataSet, dataBase));
-    const locationData = {estimates, numberOfSources, percentileRank, standardError}
+    const locationData = goveranceIndicatorCleaner(estimates, numberOfSources, percentileRank, standardError)
     dispatch(fetchInitialLocationSuccess(locationData));
   }
 }
