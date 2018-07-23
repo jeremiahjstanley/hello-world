32ed02020a81cf026d13447be697b9a295445048
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-23 10:14:03 -0600

    Create new reducer for locationData

---
+++ src/reducers/locationDataReducer.js
@@ -0,0 +1,8 @@
+ export const locationData = (state = [], action) => {
+  switch(action.type) {
+    case 'FETCH_LOCATION_DATA_SUCCESS':
+      return action.locationData;
+    default: 
+      return state;
+  }
+}
