7ca389ae5031c55aafb19c069f232c41af5026e6
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-23 16:50:48 -0600

    Move CountryInputField component functionality into ControlledForm class component

--- src/Containers/ControlledForm/ControlledForm.js
+++ src/Containers/ControlledForm/ControlledForm.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setLocation } from '../../thunks/setLocation';
+import { withRouter } from 'react-router-dom';
+import { setLocation, addComparisonLocation } from '../../actions';
+import { countries } from '../../helper/countryMetrics';
+import { fetchInitialLocation } from '../../thunks/fetchInitialLocation';
 import CountryInputField from '../CountryInputField/CountryInputField';
 import DataBaseSelectField from '../DataBaseSelectField/DataBaseSelectField';
 import DataSetSelectField from '../DataSetSelectField/DataSetSelectField';
 
 export class ControlledForm extends Component {
 
+  handleChange = (event) => {
+    const input = event.target.value;
+    const location = countries.find(country => input === country.name);
+    this.setState({location});
+  };
+
   handleSubmit = (event) => {
     event.preventDefault();
-    const { location, dataSet, dataBase} = this.props;
-    this.props.selectLocation(location, dataSet, dataBase);
+    const location = this.state.location;
+    const { dataSet, dataBase } = this.props;
+    this.props.selectLocation(location);
+    this.props.addComparisonLocation(location);
+    this.props.fetchInitialLocation(location.alpha_3, dataSet, dataBase);
+    this.props.history.push('/stats');
   };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <CountryInputField />
+        <p>I'm curious about...</p>
+        <input
+            type='text'
+            placeholder='enter a country'
+            onChange={this.handleChange}
+         /> 
+        <p>and their...</p>
         <DataBaseSelectField />
+        <p>but specifically, </p>
         <DataSetSelectField />
         <button>Search</button>
      </form>
@@ -26,13 +46,14 @@ export class ControlledForm extends Component {
 };
 
 export const mapStateToProps = (state) => ({
-  location: state.location.alpha_3,
   dataBase: state.dataBase.database_code,
   dataSet: state.dataSet.dataset_code
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-  selectLocation: (location, dataSet, dataBase) => dispatch(setLocation(location, dataSet, dataBase))
+  selectLocation: (location) => dispatch(setLocation(location)),
+  addComparisonLocation: (location) => dispatch(addComparisonLocation(location)),
+  fetchInitialLocation: (location, dataSet, dataBase) => dispatch(fetchInitialLocation(location, dataSet, dataBase))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ControlledForm);
