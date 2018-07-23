0515d2dc50b2c1863a6584e18927c5fa559b1c2a
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-23 17:01:41 -0600

    Add countries to compare to state tree

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { location } from './locationReducer';
+import { countriesToCompare } from './comparisonReducer';
 import { locationData } from './locationDataReducer';
 import { dataBase } from './dataBaseReducer';
 import { dataSet } from './dataSetReducer';
@@ -7,6 +8,7 @@ import { hasErrored } from './errorReducer'
 
 export const rootReducer = combineReducers({
   location,
+  countriesToCompare,
   dataBase,
   dataSet,
   locationData,
