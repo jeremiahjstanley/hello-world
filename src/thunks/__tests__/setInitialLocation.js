95e293b9deb5f7bd6c6d7900fb2be047399075af
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-22 21:26:41 -0600

    Create async tests for setInitialLocation thunk

---
+++ src/thunks/__tests__/setInitialLocation.js
@@ -0,0 +1,78 @@
+import { setInitialLocation } from '../setInitialLocation';
+import { fetchInitialLocationSuccess } from '../../actions'
+import { countries } from '../../helper/countryMetrics';
+import { goveranceIndicatorCleaner } from '../../helper/dataCleaner'
+import { fetchEstimates } from '../fetchEstimates';
+import { fetchNumberOfSources } from '../fetchNumberOfSources'; 
+import { fetchPercentileRank } from '../fetchPercentileRank';
+import { fetchStandardError } from '../fetchStandardError';
+
+jest.mock('../fetchEstimates');
+jest.mock('../fetchNumberOfSources');
+jest.mock('../fetchPercentileRank');
+jest.mock('../fetchStandardError');
+jest.mock('../../helper/dataCleaner');
+
+describe('setInitialLocation', () => {
+  let mockDispatch;
+  let mockLocation;
+  let mockDataSet;
+  let mockDataBase;
+  let isoAlpha3;
+
+  beforeEach(() => {
+    mockLocation = 'France';
+    mockDataSet = 'RQ';
+    mockDataBase = 'WWGI';
+    isoAlpha3 = 'FRA'
+    mockDispatch = jest.fn();
+  });
+
+  it('should dispatch fetchEstimates with the correct arguments', async () => {
+    const thunk = setInitialLocation(mockLocation, mockDataSet, mockDataBase);
+
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(fetchEstimates(isoAlpha3, mockDataSet, mockDataBase))
+  });
+
+  it('should dispatch fetchNumberOfSources with the correct arguments', async () => {
+    const thunk = setInitialLocation(mockLocation, mockDataSet, mockDataBase);
+
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(fetchNumberOfSources(isoAlpha3, mockDataSet, mockDataBase))
+  });
+
+  it('should dispatch fetchPercentileRank with the correct arguments', async () => {
+    const thunk = setInitialLocation(mockLocation, mockDataSet, mockDataBase);
+
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(fetchPercentileRank(isoAlpha3, mockDataSet, mockDataBase))
+  });
+
+  it('should dispatch fetchStandardError with the correct arguments', async () => {
+    const thunk = setInitialLocation(mockLocation, mockDataSet, mockDataBase);
+
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(fetchPercentileRank(isoAlpha3, mockDataSet, mockDataBase))
+  });
+
+  it('should call goveranceIndicatorCleaner with the correct arguments', async () => {
+    const thunk = setInitialLocation(mockLocation, mockDataSet, mockDataBase);
+
+    await thunk(mockDispatch);
+
+    expect(goveranceIndicatorCleaner).toHaveBeenCalled();
+  });
+
+  it('should dispatch fetchInitialLocationSuccess with the correct arguments', async () => {
+    const thunk = setInitialLocation(mockLocation, mockDataSet, mockDataBase);
+
+    await thunk(mockDispatch);
+
+    expect(mockDispatch).toHaveBeenCalledWith(fetchInitialLocationSuccess([]));
+  });
+})
