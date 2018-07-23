f415934e3deaaa13324ac2e4ceaea6ea38e33025
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-22 21:33:04 -0600

    Move hasErrored reducer into separate file

--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -1,13 +1,3 @@
-
-export const hasErrored = (state = false, action) => {
-  switch(action.type) {
-    case 'HAS_ERRORED':
-      return action.hasErrored
-    default:
-      return state;
-  }
-}
-
 export const initialLocation = (state = [], action) => {
   switch(action.type) {
     case 'FETCH_INITIAL_LOCATION_SUCCESS':
