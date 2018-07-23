551883bf5cc0fa8c606870f9dbfc655c1b3f24be
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-22 21:32:27 -0600

    Write tests for locationReducer

--- src/reducers/__tests__/locationReducer.js
+++ src/reducers/__tests__/locationReducer.js
@@ -1,31 +1,19 @@
-import { locationReducer } from '../locationReducer';
+import { initialLocation } from '../locationReducer';
 import * as actions from '../../actions';
 
 describe('locationReducer', () => {
   it('should return the initial state', () => {
     const expected = [];
 
-    const actual = locationReducer(undefined, []);
+    const actual = initialLocation(undefined, {});
 
-    expect(actual).toEqual(expected)
+    expect(actual).toEqual(expected);
   })
   it('should return the state with an intial location', () => {
-    const initialLocation;
+    const expected = 'Canada'
 
-    const expected = initialLocation;
-
-    const actual = locationReducer(undefined, actions.fetchInitialLocationSuccess(text, id))
+    const actual = initialLocation(undefined, actions.fetchInitialLocationSuccess('Canada'));
     
-    expect(actual).toEqual(expected) 
-  })
-  it('should toggle the completed property of a todo', () => {
-    const text = 'learn redux';
-    const id = 3;
-    const intialState = [{text, id, completed: false}]
-    const expected = [{text, id, completed: true}]
-
-    const results = locationReducer(intialState, actions.toggleTodo(id))
-
-    expect(results).toEqual(expected) 
+    expect(actual).toEqual(expected); 
   })
 })
