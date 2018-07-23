24a56a889c6f4b7be5ef354096c66762eae83d34
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-23 10:03:44 -0600

    Add setLocation, setDataBase, setDataSet, and fetchLocationDataSuccess actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,26 @@
+export const setLocation = (location) => ({
+  type: 'SET_LOCATION',
+  location
+});
+
+export const setDataBase= (dataBase) => ({
+  type: 'SET_DATABASE',
+  dataBase
+});
+
+export const setDataSet= (dataSet) => ({
+  type: 'SET_DATASET',
+  dataSet
+});
+
+export const fetchLocationDataSuccess = (locationData) => ({
+  type: 'FETCH_LOCATION_DATA_SUCCESS',
+  locationData
+});
+
 export const hasErrored = (status) => ({
   type: 'HAS_ERRORED',
   hasErrored: status
 });
 
-export const fetchInitialLocationSuccess = (initialLocation) => ({
-  type: 'FETCH_INITIAL_LOCATION_SUCCESS',
-  initialLocation
-});
+
