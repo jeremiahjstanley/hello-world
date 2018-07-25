39a3a372f0edad6ffeb98a0a6fb82253184af3e6
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-25 00:41:25 -0600

    Change locationData reducer to have initial state of empty object

--- src/reducers/locationDataReducer.js
+++ src/reducers/locationDataReducer.js
@@ -1,4 +1,4 @@
- export const locationData = (state = [], action) => {
+ export const locationData = (state = {}, action) => {
   switch(action.type) {
     case 'FETCH_LOCATION_DATA_SUCCESS':
       return action.locationData;
