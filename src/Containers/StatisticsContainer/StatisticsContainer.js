20ded849f0dfc5d5b7fdd1dd3e31c424acae40d1
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-25 00:32:28 -0600

    Remove unnecessary props, remove router Link, leave comments for future conditional rendering

--- src/Containers/StatisticsContainer/StatisticsContainer.js
+++ src/Containers/StatisticsContainer/StatisticsContainer.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom'; 
 import GovernanceIndicatorsStatistics from '../GovernanceIndicatorsStatistics/GovernanceIndicatorsStatistics';
 import './StatisticsContainer.css'
 
 
-export const  StatisticsContainer = ({location, dataBase, dataSet, locationData}) => {
+export const  StatisticsContainer = ({dataBase}) => {
   return (
     <div>
-      <h1>{dataBase.name} - {dataSet.name}</h1>
       <GovernanceIndicatorsStatistics/>
-      <Link to='/stats/compare'>
-        See how {location.name} stacks up against...
-      </Link>
     </div>
   )
 }
 
 export const mapStateToProps = (state) => ({
-  location: state.location,
   dataBase: state.dataBase,
-  dataSet: state.dataSet,
-  locationData: state.locationData
 });
 
-export default withRouter(connect(mapStateToProps)(StatisticsContainer));
+// create switch statement for WWDI figures
+
+export default connect(mapStateToProps)(StatisticsContainer);
