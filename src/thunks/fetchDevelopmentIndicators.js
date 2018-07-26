e9fed4855c4322a1c5aace2e94f64a331f8d00ee
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-25 18:25:03 -0600

    Rename fetchLocation thunk and test, fetchGovernanceIndicators

---
+++ src/thunks/fetchDevelopmentIndicators.js
@@ -0,0 +1,27 @@
+import { fetchLocationDataSuccess } from '../actions';
+import { dataCleaner } from '../helper/dataCleaner';
+import { hasErrored } from '../actions';
+import { key } from '../helper/apiKey';
+
+export const fetchDevelopmentIndicators = (locations, dataSet, dataBase) => {
+    return async (dispatch) => {
+        const unresolvedDevelopmentIndicators = locations.map(async location => {
+            const isoAlpha3 = location.alpha_3
+            try {
+              const url = `https://www.quandl.com/api/v3/datasets/${dataBase}/${isoAlpha3}_${dataSet}.json?api_key=${key}`;
+              const response = await fetch(url);
+              if (!response.ok) {
+                throw Error(response.statusText);
+              }
+              return await response.json();
+            } catch (error) {
+              dispatch(hasErrored(true));
+            }
+        });
+        const developmentIndicators = await Promise.all(unresolvedDevelopmentIndicators);
+        const cleanDevelopmentIndicators = developmentIndicators.map(indicator => {
+            return dataCleaner(indicator);
+        });
+        await dispatch(fetchLocationDataSuccess(cleanDevelopmentIndicators))   
+    }
+};
