734ec12392caae51dce709e082d207375f01cbc4
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-26 17:11:02 -0600

    Lint ChangeDateSetForm.js file and clean up snytax

--- src/Containers/ChangeDataSetForm/ChangeDataSetForm.js
+++ src/Containers/ChangeDataSetForm/ChangeDataSetForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import { fetchGovernanceIndicators } from '../../thunks/fetchGovernanceIndicators';
+import { fetchDevelopmentIndicators } from '../../thunks/fetchDevelopmentIndicators';
 import { DataSetSelectField } from '../DataSetSelectField/DataSetSelectField';
 
 export class ChangeDataSetForm extends Component {
@@ -9,12 +10,16 @@ export class ChangeDataSetForm extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     const { location, dataSet, dataBase} = this.props;
-    this.props.fetchGovernanceIndicators(location, dataSet.dataset_code, dataBase.database_code);
+    ((dataBase === 'WWGI') ? 
+      this.props.fetchGovernanceIndicators(location, dataSet, dataBase):
+      this.props.fetchDevelopmentIndicators(location, dataSet, dataBase));
     this.props.history.push('/stats');
   };
 
   render() {
+
     return (
+
       <div className='change-form-container'>
         <Link to='/stats' className='back-button'>◀ back</Link>
         <form onSubmit={this.handleSubmit} className='change-form'>
@@ -22,18 +27,20 @@ export class ChangeDataSetForm extends Component {
           <button className='change-form-button'>Change</button>
        </form>
      </div>
-    )
+
+    );
   };
 };
 
 export const mapStateToProps = (state) => ({
-  location: state.location,
-  dataBase: state.dataBase,
-  dataSet: state.dataSet
+  dataBase: state.dataBase.database_code,
+  dataSet: state.dataSet.dataset_code,
+  location: state.location
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-  fetchGovernanceIndicators: (locations, dataBase, dataSet) => dispatch(fetchGovernanceIndicators(locations, dataBase, dataSet))
+  fetchGovernanceIndicators: (locations, dataBase, dataSet) => dispatch(fetchGovernanceIndicators(locations, dataBase, dataSet)),
+  fetchDevelopmentIndicators: (locations, dataBase, dataSet) => dispatch(fetchDevelopmentIndicators(locations, dataBase, dataSet))
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ChangeDataSetForm));
