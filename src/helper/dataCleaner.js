3d52046928610ae2435d27caba27471d256937ce
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-26 17:20:59 -0600

    Add .reverse to display data chronologically

--- src/helper/dataCleaner.js
+++ src/helper/dataCleaner.js
@@ -1,6 +1,6 @@
 export const dataCleaner = (graphData) => {
     const data = graphData.dataset.data.map(dataPoint => {
        return {x: dataPoint[0].substring(0, 4), y: dataPoint[1]}
-    })
+    }).reverse();
     return data;
-}
+};
