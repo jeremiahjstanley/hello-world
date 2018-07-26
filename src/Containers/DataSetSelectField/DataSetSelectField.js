c2675afb02ecca57f850ec1cc166ab258a297d0e
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-26 17:14:52 -0600

    Lint DataSetSelectField.js and fix syntax errors

--- src/Containers/DataSetSelectField/DataSetSelectField.js
+++ src/Containers/DataSetSelectField/DataSetSelectField.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setDataSet } from '../../actions';
+import { setDataSet, changeData } from '../../actions';
 import { dataMetrics } from '../../helper/dataMetrics';
 
 export class DataSetSelectField extends Component {
+  
   constructor() {
     super()
 
     this.state = {
     };
-  }
+
+  };
 
   selectMetric = (event) => {
     const dataSet = this.state.dataBase.datasets.find(dataset => event.target.value === dataset.name);
     this.props.selectDataSet({...dataSet});
-  }
+  };
 
   static getDerivedStateFromProps(props, state) {
     console.log(props)
@@ -23,26 +25,34 @@ export class DataSetSelectField extends Component {
       return {
         dataBase: dataMetrics.find(metric => props.dataBase === metric.name),
       };
-    }
+    };
     return state
-  }
+  };
 
   render() {
+
     if (this.state.dataBase) {
+
       const options = this.state.dataBase.datasets.map((metric, index) => (
         <option key={index}>
           { metric.name }
         </option>
       ));
+
       return (
+
         <select onChange={this.selectMetric}>
           { options }
         </select>
-      ) 
+      ); 
+
     } else
+
     return (
+
       <p className='loading-text'>Loading...</p>
-    )
+
+    );
   };
 }
 
@@ -51,6 +61,7 @@ export const mapStateToProps = (state) => ({
 })
 
 export const mapDispatchToProps = (dispatch) => ({
+  changeDataSet: (bool) => dispatch(changeData(bool)),
   selectDataSet: (dataSet) => dispatch(setDataSet(dataSet))
 })
 
