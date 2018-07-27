d788eea55bd6a00517a6af030aee4c22bd8e96a6
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-27 13:43:26 -0600

    Add label field and error handling to data cleaner

--- src/helper/dataCleaner.js
+++ src/helper/dataCleaner.js
@@ -1,6 +1,14 @@
 export const dataCleaner = (graphData) => {
+	if (!graphData.dataset.data.length) {
+		return
+	}
     const data = graphData.dataset.data.map(dataPoint => {
-       return {x: dataPoint[0].substring(0, 4), y: dataPoint[1]}
+       return {x: dataPoint[0].substring(0, 4), 
+       		     y: dataPoint[1], 
+       		     label: (
+       		     	`year: ${dataPoint[0].substring(0, 4)} 
+       		     	value: ${dataPoint[1]}`
+       		     	)}
     }).reverse();
     return data;
 };
