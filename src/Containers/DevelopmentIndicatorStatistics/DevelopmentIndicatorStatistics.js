c224e113924db1ead05d8430da38010bcc9715d8
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-30 17:24:31 -0600

    Add propType validation to DataSetSelectField component, change victory graph to use victory group and scatter components

--- src/Containers/DevelopmentIndicatorStatistics/DevelopmentIndicatorStatistics.js
+++ src/Containers/DevelopmentIndicatorStatistics/DevelopmentIndicatorStatistics.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { VictoryLine, VictoryContainer, VictoryVoronoiContainer, VictoryChart, VictoryTooltip, VictoryTheme, VictoryAxis } from 'victory';
+import PropTypes from 'prop-types';
+import { VictoryLine, VictoryContainer, VictoryVoronoiContainer, VictoryGroup, VictoryScatter, VictoryChart, VictoryTooltip, VictoryTheme, VictoryAxis } from 'victory';
 import { colors } from '../../helper/colors';
 import Loader from '../Loader/Loader'
 
@@ -31,25 +32,30 @@ export class DevelopmentIndicatorStatistics extends Component {
       const developmentChart = this.state.locationData.map((location, index) => {
    
         return ( 
+
+          <VictoryGroup
+            key={index}
+            color={colors[index]}
+            labelComponent={
+              <VictoryTooltip
+                cornerRadius={0}
+                pointerLength={20}
+                style={{fontSize: 10, fontFamily: 'Avenir Next'}}
+                flyoutStyle={{ stroke: 'black', fill: 'white'}}
+              />
+            }
+            data={location}
+          >
             <VictoryLine
               containerComponent={<VictoryContainer responsive={true}/>}
-              key={index}
-              labelComponent={
-                <VictoryTooltip
-                  cornerRadius={0}
-                  pointerLength={20}
-                  style={{fontSize: 10, fontFamily: 'Avenir Next'}}
-                  flyoutStyle={{ stroke: 'black', fill: 'white'}}
-                />
-              }
-              style={{
-                data: { stroke: colors[index] },
-                parent: { border: '1px solid #ccc'},
-              }}
-              data={location}
             />
-          );
-        });
+            <VictoryScatter
+              size={1}
+            />
+          </VictoryGroup>
+          
+        );
+      });
 
 
       return (
@@ -65,19 +71,20 @@ export class DevelopmentIndicatorStatistics extends Component {
             theme={VictoryTheme.grayscale}
             containerComponent={<VictoryVoronoiContainer />}
           >
+
             <VictoryAxis
               label={`${this.props.dataSet.name} - ${this.props.location.map(location => location.name).join(', ')}`}
               style={{
                 axisLabel: { fontSize: 10, fontFamily: 'Avenir Next', padding: 30 },
                 tickLabels: {fontSize: 6, fontFamily: 'Avenir Next', padding: 10, angle: 90}
               }}
-              />
+            />
 
-              <VictoryAxis dependentAxis
-                style={{
-                  axisLabel: {fontSize: 10, fontFamily: 'Avenir Next', padding: 40, angle: 90},
-                  tickLabels: {fontSize: 6, fontFamily: 'Avenir Next', padding: 10}
-                }}
+            <VictoryAxis dependentAxis
+              style={{
+                axisLabel: {fontSize: 10, fontFamily: 'Avenir Next', padding: 40, angle: 90},
+                tickLabels: {fontSize: 6, fontFamily: 'Avenir Next', padding: 10}
+              }}
             />
 
             { developmentChart }
@@ -105,5 +112,12 @@ export const mapStateToProps = (state) => ({
   location: state.location,
   locationData: state.locationData
 });
+
+DevelopmentIndicatorStatistics.propTypes = {
+  dataBase: PropTypes.object.isRequired,
+  dataSet: PropTypes.object.isRequired,
+  location: PropTypes.array.isRequired,
+  locationData: PropTypes.array.isRequired
+};
 
 export default connect(mapStateToProps)(DevelopmentIndicatorStatistics);
