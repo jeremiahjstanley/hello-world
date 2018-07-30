e7874f608cde56f7b0d62b9b76feb10bba131109
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-30 17:22:04 -0600

    Add propType validation to DataBaseSelectField component

--- src/Containers/DataBaseSelectField/DataBaseSelectField.js
+++ src/Containers/DataBaseSelectField/DataBaseSelectField.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { setDataBase } from '../../actions';
 import { dataMetrics } from '../../helper/dataMetrics';
 
@@ -31,5 +32,9 @@ export class DataBaseSelectField extends Component {
 export const mapDispatchToProps = (dispatch) => ({
   selectDataBase: (dataBase) => dispatch(setDataBase(dataBase))
 });
+
+DataBaseSelectField.propTypes = {
+  selectDataBase: PropTypes.func.isRequired
+};
 
 export default connect(null, mapDispatchToProps)(DataBaseSelectField);
