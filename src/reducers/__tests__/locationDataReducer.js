532e5faf10c79c5c45a6ce0dfaf9b58f5ce1abc8
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-30 17:37:38 -0600

    Change default state to array to reflect changes in action creator, add semicolons and stantardize quotes for code sanitation

--- src/reducers/__tests__/locationDataReducer.js
+++ src/reducers/__tests__/locationDataReducer.js
@@ -2,26 +2,33 @@ import { locationData } from '../locationDataReducer';
 import * as actions from '../../actions';
 
 describe('locationDataReducer', () => {
-  it('should return the initial state', () => {
-    const expected = {};
 
-    const actual = locationData(undefined, {});
+  it('should return the initial state', () => {
+
+    const expected = [];
+
+    const actual = locationData(undefined, []);
 
     expect(actual).toEqual(expected);
-  })
+
+  });
+
   it('should return the state with a locationData array', () => {
-    const expected = {
-        "name": "2016-12-31", 
-        "Estimate": 1.3533138036728,
-        "Number of Sources": 10,
-        "Percentile Rank": 95.566505432129,
-        "Standard Error": 0.13472333550453,
-      }
+
+    const expected = [{
+        'name': "2016-12-31", 
+        'Estimate': 1.3533138036728,
+        'Number of Sources': 10,
+        'Percentile Rank': 95.566505432129,
+        'Standard Error': 0.13472333550453,
+      }];
 
     const actual = locationData(undefined, actions.fetchLocationDataSuccess(expected));
     
     expect(actual).toEqual(expected); 
-  })
-})
+
+  });
+
+});
 
 
