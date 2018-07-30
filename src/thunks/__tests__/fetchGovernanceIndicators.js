048fbd5d7858b5e259eef962cdf5ae1866f29631
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-30 17:43:06 -0600

    Add more robust mock data, add semicolons for code sanitation

--- src/thunks/__tests__/fetchGovernanceIndicators.js
+++ src/thunks/__tests__/fetchGovernanceIndicators.js
@@ -14,6 +14,7 @@ jest.mock('../fetchStandardError');
 jest.mock('../../helper/dataCleaner');
 
 describe('fetchGovernanceIndicators', () => {
+  
   let mockDispatch;
   let mockLocation;
   let mockDataSet;
@@ -21,58 +22,95 @@ describe('fetchGovernanceIndicators', () => {
   let isoAlpha3;
 
   beforeEach(() => {
-    mockLocation = ['France'];
+
+    mockLocation = [{
+      name:'Burkina Faso',
+      alpha_2:'BF',
+      alpha_3:'BFA',
+      country_code:'854',
+      iso_3166_2:'ISO 3166-2: BF',
+      region:'Africa',
+      sub_region:'Sub-Saharan Africa',
+      intermediate_region:'Western Africa',
+      region_code:'002',sub_region_code:'202',
+      intermediate_region_code:'011'
+    }];
+
     mockDataSet = 'RQ';
+
     mockDataBase = 'WWGI';
-    isoAlpha3 = 'FRA'
+
+    isoAlpha3 = 'BFA'
+
     mockDispatch = jest.fn();
+
   });
 
   it('should dispatch fetchEstimates with the correct arguments', async () => {
+
     const thunk = fetchGovernanceIndicators(mockLocation, mockDataSet, mockDataBase);
 
     await thunk(mockDispatch);
 
-    expect(mockDispatch).toHaveBeenCalledWith(fetchEstimates(isoAlpha3, mockDataSet, mockDataBase))
+    expect(mockDispatch).toHaveBeenCalledWith(fetchEstimates(isoAlpha3, mockDataSet, mockDataBase));
+
   });
 
   it('should dispatch fetchNumberOfSources with the correct arguments', async () => {
+
     const thunk = fetchGovernanceIndicators(mockLocation, mockDataSet, mockDataBase);
 
     await thunk(mockDispatch);
 
-    expect(mockDispatch).toHaveBeenCalledWith(fetchNumberOfSources(isoAlpha3, mockDataSet, mockDataBase))
+    expect(mockDispatch).toHaveBeenCalledWith(fetchNumberOfSources(isoAlpha3, mockDataSet, mockDataBase));
+
   });
 
   it('should dispatch fetchPercentileRank with the correct arguments', async () => {
+
     const thunk = fetchGovernanceIndicators(mockLocation, mockDataSet, mockDataBase);
 
     await thunk(mockDispatch);
 
-    expect(mockDispatch).toHaveBeenCalledWith(fetchPercentileRank(isoAlpha3, mockDataSet, mockDataBase))
+    expect(mockDispatch).toHaveBeenCalledWith(fetchPercentileRank(isoAlpha3, mockDataSet, mockDataBase));
+
   });
 
   it('should dispatch fetchStandardError with the correct arguments', async () => {
+
     const thunk = fetchGovernanceIndicators(mockLocation, mockDataSet, mockDataBase);
 
     await thunk(mockDispatch);
 
-    expect(mockDispatch).toHaveBeenCalledWith(fetchPercentileRank(isoAlpha3, mockDataSet, mockDataBase))
+    expect(mockDispatch).toHaveBeenCalledWith(fetchPercentileRank(isoAlpha3, mockDataSet, mockDataBase));
+
   });
 
-  it('should call dataCleaner with the correct arguments', async () => {
+  it('should call dataCleaner function', async () => {
+
     const thunk = fetchGovernanceIndicators(mockLocation, mockDataSet, mockDataBase);
 
     await thunk(mockDispatch);
 
     expect(dataCleaner).toHaveBeenCalled();
+
   });
 
   it('should dispatch fetchLocationDataSuccess with the correct arguments', async () => {
+
     const thunk = fetchGovernanceIndicators(mockLocation, mockDataSet, mockDataBase);
 
     await thunk(mockDispatch);
 
-    expect(mockDispatch).toHaveBeenCalledWith(fetchLocationDataSuccess([]));
+    const args = [{
+      cleanEstimates: [], 
+      cleanNumberOfSources: [],
+      cleanPercentileRank: [],
+      cleanStandardError: []
+    }];
+
+    expect(mockDispatch).toHaveBeenCalledWith(fetchLocationDataSuccess(args));
+
   });
+
 })
