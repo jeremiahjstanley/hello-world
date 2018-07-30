164fd91ab4a59206dd775f4ec7920ebf0517c7ca
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-30 17:34:49 -0600

    Add linebreak on label property

--- src/helper/dataCleaner.js
+++ src/helper/dataCleaner.js
@@ -6,8 +6,8 @@ export const dataCleaner = (graphData) => {
        return {x: dataPoint[0].substring(0, 4), 
        		     y: dataPoint[1], 
        		     label: (
-       		     	`year: ${dataPoint[0].substring(0, 4)} 
-       		     	value: ${dataPoint[1]}`
+       		     	`year: ${dataPoint[0].substring(0, 4)}
+      value: ${dataPoint[1]}`
        		     	)}
     }).reverse();
     return data;
