f3cb6257dc738e1ea142819e5b028c2dc7e05dd4
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-30 17:15:47 -0600

    Make stats protected route based on newly added locationData prop, create route and redirect for new ErrorPage component

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route, withRouter } from 'react-router-dom';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import ExplanationText from '../ExplanationText/ExplanationText';
+import ErrorPage from '../ErrorPage/ErrorPage';
 import AddComparisonForm from '../AddComparisonForm/AddComparisonForm';
 import GraphLinks from '../GraphLinks/GraphLinks';
 import ControlledForm from '../ControlledForm/ControlledForm';
@@ -16,27 +18,52 @@ export class App extends Component {
 
     return (
       
-      <div className="App">
-        <header className="App-header">
-        <h1 className="App-title">Hello, World</h1>
+      <div className='App'>
+        <header className='App-header'>
+        <h1 className='App-title'>Hello, World</h1>
         </header>
         <Route path='/why' component={ExplanationText}/>
         <Route exact path='/' component={ControlledForm}/>
-        { this.props.hasErrored ? <p>Sorry! There was an error loading your result.</p> : ''}
-        <Route exact path='/stats' component={GraphLinks}/>
+        <Route exact path='/404' component={ErrorPage}/>
+
+        { this.props.hasErrored ? <Redirect to='/404'/> : ''}
+
         <Route path='/stats/compare' component={AddComparisonForm}/>
         <Route path='/stats/change_data_base' component={ChangeDataBaseForm}/>
         <Route path='/stats/change_data_set' component={ChangeDataSetForm}/>
-        <Route path='/stats' component={StatisticsContainer}/>
+
+        <Route path="/stats" render={() => (
+          this.props.locationData.length ? (
+            <GraphLinks/>
+            ) : (
+            <Redirect to='/'/>
+            )
+          )}
+        />
+
+        <Route path="/stats" render={() => (
+          this.props.locationData.length ? (
+            <StatisticsContainer/>
+            ) : (
+            <Redirect to='/'/>
+            )
+          )}
+        />
+
       </div>
 
     );
   };
 };
 
-const mapStateToProps = (state) => ({
+export const mapStateToProps = (state) => ({
   hasErrored: state.hasErrored,
+  locationData: state.locationData
 });
+
+App.propTypes = {
+  hasErrored: PropTypes.bool.isRequired,
+};
 
 export default withRouter(connect(mapStateToProps)(App));
 
