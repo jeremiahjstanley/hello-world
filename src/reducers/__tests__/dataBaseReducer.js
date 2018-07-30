5d0450fdc2764a4717f199d1609c45bc1f55b94a
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-30 17:34:04 -0600

    Add empty lines for readability

--- src/reducers/__tests__/dataBaseReducer.js
+++ src/reducers/__tests__/dataBaseReducer.js
@@ -2,18 +2,25 @@ import { dataBase } from '../dataBaseReducer';
 import * as actions from '../../actions';
 
 describe('dataBaseReducer', () => {
+
   it('should return the initial state', () => {
+
     const expected = {};
 
     const actual = dataBase(undefined, {});
 
     expect(actual).toEqual(expected);
-  })
+
+  });
+
   it('should return the state with an dataBase object', () => {
+
     const expected = {name:'Worldwide Governance Indicators', database_code:'WWGI'};
 
     const actual = dataBase(undefined, actions.setDataBase(expected));
     
     expect(actual).toEqual(expected); 
-  })
-})
+
+  });
+
+});
