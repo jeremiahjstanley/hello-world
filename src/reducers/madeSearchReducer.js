905aa5bf02df165d16bd4084e9f4b7c9a8a1926a
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-31 12:09:43 -0600

    Create madeSearchReducer

---
+++ src/reducers/madeSearchReducer.js
@@ -0,0 +1,8 @@
+export const madeSearch = (state = false, action) => {
+  switch(action.type) {
+    case 'MADE_SEARCH':
+      return action.madeSearch;
+    default: 
+      return state;
+  }
+};
