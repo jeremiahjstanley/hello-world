9369d374cc2031e55db90804a523d1d96cec8c45
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-31 18:25:45 -0600

    Lint fetchDevelopmentIndicators thunk

--- src/thunks/fetchDevelopmentIndicators.js
+++ src/thunks/fetchDevelopmentIndicators.js
@@ -1,30 +1,29 @@
-import { fetchLocationDataSuccess } from '../actions';
+import { fetchLocationDataSuccess, hasErrored  } from '../actions';
 import { dataCleaner } from '../helper/dataCleaner';
-import { hasErrored } from '../actions';
 import { key } from '../helper/apiKey';
 
 export const fetchDevelopmentIndicators = (locations, dataSet, dataBase) => {
 
-    return async (dispatch) => {
-        const unresolvedDevelopmentIndicators = locations.map(async location => {
-            const isoAlpha3 = location.alpha_3
-            try {
-              const url = `https://www.quandl.com/api/v3/datasets/${dataBase}/${isoAlpha3}_${dataSet}.json?api_key=${key}`;
-              const response = await fetch(url);
-              if (!response.ok) {
-                throw Error(response.statusText);
-              }
-              return await response.json();
-            } catch (error) {
-              dispatch(hasErrored(true));
-            }
-        });
+  return async (dispatch) => {
+    const unresolvedDevelopmentIndicators = locations.map(async location => {
+      const isoAlpha3 = location.alpha_3;
+      try {
+        const url = `https://www.quandl.com/api/v3/datasets/${dataBase}/${isoAlpha3}_${dataSet}.json?api_key=${key}`;
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return await response.json();
+      } catch (error) {
+        dispatch(hasErrored(true));
+      }
+    });
 
-        const developmentIndicators = await Promise.all(unresolvedDevelopmentIndicators);
-        const cleanDevelopmentIndicators = developmentIndicators.map(indicator => {
-            return dataCleaner(indicator);
-        });
-        
-        await dispatch(fetchLocationDataSuccess(cleanDevelopmentIndicators))   
-    };
+    const developmentIndicators = await Promise.all(unresolvedDevelopmentIndicators);
+    const cleanDevelopmentIndicators = developmentIndicators.map(indicator => {
+      return dataCleaner(indicator);
+    });
+    
+    await dispatch(fetchLocationDataSuccess(cleanDevelopmentIndicators));  
+  };
 };
