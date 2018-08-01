f8f22c06783e05153ceeeee5bda009cd2baa63d4
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-31 18:25:09 -0600

    Lint fetchGovernanceIndicators thunk

--- src/thunks/fetchGovernanceIndicators.js
+++ src/thunks/fetchGovernanceIndicators.js
@@ -6,20 +6,20 @@ import { fetchPercentileRank } from './fetchPercentileRank';
 import { fetchStandardError } from './fetchStandardError';
 
 export const fetchGovernanceIndicators = (locations, dataSet, dataBase) => {
-    return async (dispatch) => {
-        const unresolvedGovernanceIndicators = locations.map(async location => {
-            const alpha3 = location.alpha_3
-            const estimates = await dispatch(fetchEstimates(alpha3, dataSet, dataBase));
-            const numberOfSources = await dispatch(fetchNumberOfSources(alpha3, dataSet, dataBase)); 
-            const percentileRank = await dispatch(fetchPercentileRank(alpha3, dataSet, dataBase));
-            const standardError = await dispatch(fetchStandardError(alpha3, dataSet, dataBase));
-            const cleanEstimates = dataCleaner(estimates);
-            const cleanNumberOfSources = dataCleaner(numberOfSources);
-            const cleanPercentileRank = dataCleaner(percentileRank);
-            const cleanStandardError = dataCleaner(standardError);
-            return {cleanEstimates, cleanNumberOfSources, cleanPercentileRank, cleanStandardError};
-        });
-        const governanceIndicators = await Promise.all(unresolvedGovernanceIndicators)
-        await dispatch(fetchLocationDataSuccess(governanceIndicators))     
-    };
+  return async (dispatch) => {
+    const unresolvedGovernanceIndicators = locations.map(async location => {
+      const alpha3 = location.alpha_3;
+      const estimates = await dispatch(fetchEstimates(alpha3, dataSet, dataBase));
+      const numberOfSources = await dispatch(fetchNumberOfSources(alpha3, dataSet, dataBase)); 
+      const percentileRank = await dispatch(fetchPercentileRank(alpha3, dataSet, dataBase));
+      const standardError = await dispatch(fetchStandardError(alpha3, dataSet, dataBase));
+      const cleanEstimates = dataCleaner(estimates);
+      const cleanNumberOfSources = dataCleaner(numberOfSources);
+      const cleanPercentileRank = dataCleaner(percentileRank);
+      const cleanStandardError = dataCleaner(standardError);
+      return {cleanEstimates, cleanNumberOfSources, cleanPercentileRank, cleanStandardError};
+    });
+    const governanceIndicators = await Promise.all(unresolvedGovernanceIndicators);
+    await dispatch(fetchLocationDataSuccess(governanceIndicators));
+  };
 };
